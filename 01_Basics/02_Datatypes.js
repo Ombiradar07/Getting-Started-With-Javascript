58c85393ee3c73a5828b3c2dbc090b3b52b5c953
Omkar Biradar <141556018+Ombiradar07@users.noreply.github.com>
2023-08-15 08:21:50 +0000

    Learnerd About Data types in JS

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code an newer version different from old code
+
+// alert(5-5) => it will return popup with result 0 in browser
+//            => here it will give error because we are using node js , not browser
+
+
+let name = "Omkar Biradar"
+let age  = 20
+let loginStatus = true;
+/* 
+    Data Types In Js 
+
+    number    => approx range 2 to the power of 53
+    bigInt    => rarely used (used to store a very  big number)
+    string    => "",''
+    boolean   => true /false
+    null      => standalone value of type object 
+    undefined => undefined type value
+    symbol    => unique
+
+
+    object (Anything in Javascript is Object)
+
+*/
+
+
+console.log(typeof null) //     => Object
+console.log(typeof undefined)// => Undefined 
